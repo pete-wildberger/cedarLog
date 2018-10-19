3d25ddb4eafc13938b378cf0be924a0d3b27f071
Peter Wildberger <pwildberger@prim.com>
2018-10-19 17:58:20 -0500

    added shifts table definition

--- db/seed.sql
+++ db/seed.sql
@@ -11,3 +11,22 @@ CREATE TABLE events (
 
 CREATE UNIQUE INDEX untitled_table_pkey ON events(_id int4_ops);
 CREATE UNIQUE INDEX events_title_key ON events(title text_ops);
+
+-- Table Definition ----------------------------------------------
+
+CREATE TABLE shifts (
+    _id SERIAL PRIMARY KEY,
+    event_id integer NOT NULL,
+    user_id integer NOT NULL,
+    shift character varying(32) NOT NULL,
+    deleted boolean NOT NULL DEFAULT false,
+    approved boolean NOT NULL DEFAULT false,
+    approved_by integer,
+    approved_at timestamp without time zone,
+    created_at timestamp without time zone NOT NULL DEFAULT now(),
+    comment text
+);
+
+-- Indices -------------------------------------------------------
+
+CREATE UNIQUE INDEX shifts_pkey ON shifts(_id int4_ops);
