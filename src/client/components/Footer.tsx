91ed0266ba3efbb2f19751649bae371cec75fff6
Peter Wildberger <pete.wildberger@gmail.com>
2019-03-26 18:40:40 -0500

    webpack change plus pure component addition

--- src/client/components/Footer.tsx
+++ src/client/components/Footer.tsx
@@ -1,40 +1,27 @@
-import * as React from "react";
+import * as React from 'react';
 
-export const Footer = () => {
-  return (
-    <div>
-      <div className="social">
-        <nav className="social-container">
-          <a href="https://www.facebook.com/thecedar" target="_blank">
-            <img
-              className="social-icon"
-              src="./assets/img/if_facebook_3009513.svg"
-              alt="facebook"
-            />
-          </a>
-          <a href="http://instagram.com/thecedar" target="_blank">
-            <img
-              className="social-icon"
-              src="./assets/img/if_instagram_3009537.svg"
-              alt="insta"
-            />
-          </a>
-          <a href="http://twitter.com/TheCedar" target="_blank">
-            <img
-              className="social-icon"
-              src="./assets/img/if_twitter_3009565.svg"
-              alt="twitter"
-            />
-          </a>
-        </nav>
-      </div>
-      <div className="foot">
-        <span className="site-address">
-          The Cedar Cultural Center, 416 Cedar Avenue South, Minneapolis, MN,
-          55454
-        </span>
-        <span className="site-address">(612) 338-2674</span>
-      </div>
-    </div>
-  );
-};
+export class Footer extends React.PureComponent {
+	render() {
+		return (
+			<div>
+				<div className="social">
+					<nav className="social-container">
+						<a href="https://www.facebook.com/thecedar" target="_blank">
+							<img className="social-icon" src="./assets/img/if_facebook_3009513.svg" alt="facebook" />
+						</a>
+						<a href="http://instagram.com/thecedar" target="_blank">
+							<img className="social-icon" src="./assets/img/if_instagram_3009537.svg" alt="insta" />
+						</a>
+						<a href="http://twitter.com/TheCedar" target="_blank">
+							<img className="social-icon" src="./assets/img/if_twitter_3009565.svg" alt="twitter" />
+						</a>
+					</nav>
+				</div>
+				<div className="foot">
+					<span className="site-address">The Cedar Cultural Center, 416 Cedar Avenue South, Minneapolis, MN, 55454</span>
+					<span className="site-address">(612) 338-2674</span>
+				</div>
+			</div>
+		);
+	}
+}
